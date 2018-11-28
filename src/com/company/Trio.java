edcbc9255c22810689dee26870ebf7aaad23310b
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2018-11-28 08:10:08 -0500

    Update Trio.java

--- src/com/company/Trio.java
+++ src/com/company/Trio.java
@@ -17,6 +17,20 @@ public class Trio implements MenuItem
     }
     public double getPrice()
     {
-        return sandwich.getPrice() + salad.getPrice() + drink.getPrice();
+        double sandwichPrice = sandwich.getPrice();
+        double saladPrice = salad.getPrice();
+        double drinkPrice = drink.getPrice();
+        if (sandwichPrice <= saladPrice && sandwichPrice <= drinkPrice)
+        {
+            return saladPrice + drinkPrice;
+        }
+        else if (saladPrice <= sandwichPrice && saladPrice <= drinkPrice)
+        {
+            return sandwichPrice + drinkPrice;
+        }
+        else
+        {
+            return sandwichPrice + saladPrice;
+        }
     }
 }
